c76b2895a6f2229f332e79e2a1496ead51572f3d
Jim <vimcoders@gmail.com>
2021-07-09 22:41:16 +0800

    tx

--- tx.go
+++ tx.go
@@ -3,19 +3,17 @@ package sqlx
 import (
 	"context"
 	"database/sql"
-
-	"github.com/vimcoders/go-driver"
 )
 
 type Tx struct {
 	*sql.Tx
 }
 
-func (tx *Tx) Exec(list ...interface{}) (driver.Result, error) {
+func (tx *Tx) Exec(list ...interface{}) (interface{}, error) {
 	return tx.Exec(context.Background(), list)
 }
 
-func (tx *Tx) ExecContext(ctx context.Context, list ...interface{}) (driver.Result, error) {
+func (tx *Tx) ExecContext(ctx context.Context, list ...interface{}) (interface{}, error) {
 	for _, i := range list {
 		convert, ok := i.(Convertor)
 
